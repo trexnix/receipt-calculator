51c9bc8469e25e822722e66e90928befdde2b57d
Trexnix <trexnix@gmail.com>
2017-09-29 20:50:53 +0700

    Implement TaxCalculator class

--- lib/receptionist/tax_calculator.rb
+++ lib/receptionist/tax_calculator.rb
@@ -1,5 +1,48 @@
 module Receptionist
   class TaxCalculator
+    attr_accessor :code_to_rate, :nearest_decimal
     
+    def initialize
+      @code_to_rate = {
+        none: 0,
+        basic: 0.1,
+        imported: 0.05
+      }
+      @nearest_decimal = 0.05
+    end
+
+    def calculate_tax(product)
+      tax = product.price * get_tax_rate_by_product(product)
+      (tax / nearest_decimal).round * nearest_decimal
+    end
+
+    def is_basic_tax_exampt?(product)
+      list = [
+        "book",
+        "chocolate bar",
+        "imported box of chocolates",
+        "packet of headache pills"
+      ]
+      list.include? product.name
+    end
+
+    def is_import_tax_exampt?(product)
+      !product.name.include? "imported"
+    end
+
+    def get_tax_rate_by_product(product)
+      map_product_to_tax_codes(product)
+        .map {|tax_code| code_to_rate[tax_code]}
+        .inject(:+)
+    end
+
+  private
+
+    def map_product_to_tax_codes(product)
+      tax_codes = []
+      tax_codes << (is_basic_tax_exampt?(product) ? :none : :basic)
+      tax_codes << (is_import_tax_exampt?(product) ? :none : :imported)
+      tax_codes
+    end
   end
 end
---
+++ spec/tax_calculator_spec.rb
@@ -0,0 +1,32 @@
+require 'receptionist/product'
+require 'receptionist/tax_calculator'
+
+RSpec.describe Receptionist::TaxCalculator do
+  before do
+    @tax_calculator = Receptionist::TaxCalculator.new
+  end
+  it "domestic books, food, mediacal products should be exampt" do
+    book = Product.new(name: "book", price: 20)
+    food = Product.new(name: "chocolate bar", price: 10)
+    medicine = Product.new(name: "packet of headache pills", price: 5)
+
+    expect(@tax_calculator.calculate_tax(book)).to eq(0)
+    expect(@tax_calculator.calculate_tax(book)).to eq(0)
+    expect(@tax_calculator.calculate_tax(book)).to eq(0)
+  end
+
+  it "imported books, food, medical products should be taxed" do
+    food = Product.new(name: "imported box of chocolates", price: 10)
+    expect(@tax_calculator.calculate_tax(food)).to eq(0.5)
+  end
+
+  it "imported products other than books, food, medical should be taxed with both basic and imported tax" do
+    perfume = Product.new(name: "imported bottle of perfume", price: 100)
+    expect(@tax_calculator.calculate_tax(perfume)).to eq(15)
+  end
+
+  it "tax should be rounded up to nearest decimal" do
+    perfume = Product.new(name: "imported bottle of perfume", price: 47.5)
+    expect(@tax_calculator.calculate_tax(perfume)).to eq(7.15)
+  end
+end
