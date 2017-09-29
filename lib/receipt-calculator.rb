8b3b6575b8ae6e48e26ebaf137aa317971a0ae07
Trexnix <trexnix@gmail.com>
2017-09-30 04:58:35 +0700

    Refactor

--- lib/receipt-calculator.rb
+++ lib/receipt-calculator.rb
@@ -6,30 +6,46 @@ require "receipt-calculator/receipt"
 module ReceiptCalculator
   extend self
 
-  def default_csv_headers
-    ['Quantity', 'Product', 'Price']
-  end
-
-  def parse(csv_content_input)
+  def parse(csv_input)
     receipt = Receipt.new
-    lines = CSV.parse(csv_content_input, headers: true, header_converters: ->(h) {h.strip})
-    lines.each do |line|
-      quantity, name, price = default_csv_headers.map {|field| line[field].strip}
-      receipt.add_item(Product.new(name: name, price: price), quantity)
+    lines = CSV.parse(csv_input, headers: true, header_converters: ->(h) {h.strip})
+    lines = lines.map do |line|
+      quantity, name, price = parse_line(line)
+      receipt.add_item(
+        Product.new(name: name, price: price),
+        quantity
+      )
     end
     receipt
   end
 
-  def print_receipt(csv_content_input)
-    receipt = parse(csv_content_input)
-    output = CSV.generate do |csv|
+  def parse_line(line)
+    default_csv_headers.map {|field| line[field].strip}
+  end
+
+  def print_receipt(csv_input)
+    receipt = parse(csv_input)
+    generate_receipt_body(receipt) + "\n" + generate_receipt_footer(receipt)
+  end
+
+  def generate_receipt_body(receipt)
+    CSV.generate do |csv|
       receipt.items.each do |receipt_item|
-        csv << [receipt_item.quantity, " #{receipt_item.product.name}", " #{receipt_item.total_price}"]
+        csv << [
+          receipt_item.quantity,
+          " #{receipt_item.product.name}",
+          " #{receipt_item.total_price}"]
       end
     end
-    output << "\n"
+  end
+
+  def generate_receipt_footer(receipt)
+    output = ""
     output << sprintf("Sales Taxes: %.2f\n", receipt.sales_taxes)
     output << sprintf("Total: %.2f", receipt.total_price)
-    output
+  end
+
+  def default_csv_headers
+    ['Quantity', 'Product', 'Price']
   end
 end
